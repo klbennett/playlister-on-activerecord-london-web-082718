bae8f4d368a40ea60ed352e424b6c11ecf535925
Kay B <kaylouisebennett@gmail.com>
2018-09-16 09:07:33 +0000

    automatically backed up by learn

--- db/migrate/01_create_songs.rb
+++ db/migrate/01_create_songs.rb
@@ -1,6 +1,7 @@
 class CreateSongs < ActiveRecord::Migration[5.2]
   def change
     create_table :songs do |t|
+      t.string :title
       t.belongs_to :artist, index: true
       t.belongs_to :genre, index: true
       t.datetime :appointment_date
