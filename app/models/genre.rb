787b45c2249a11407f353fcbe0420e176d1aa5ba
Kay B <kaylouisebennett@gmail.com>
2018-09-16 09:01:03 +0000

    automatically backed up by learn

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -1,3 +1,4 @@
-class Song < ActiveRecord::Base
-  has_many :artists, through: songs
+class Genre < ActiveRecord::Base
+  has_many :artists, through: :songs
+  has_many :genres, through: :songs
 end
