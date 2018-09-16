d5001c2ec3886f05b0f5408bce3a2219175100b4
Kay B <kaylouisebennett@gmail.com>
2018-09-16 09:10:31 +0000

    automatically backed up by learn

--- db/migrate/02_create_artists.rb
+++ db/migrate/02_create_artists.rb
@@ -1,8 +1,7 @@
 class CreateArtists < ActiveRecord::Migration[5.2]
   def change
     create_table :artists do |t|
-      t.belongs_to :artist, index: true
-      t.belongs_to :genre, index: true
+      t.string :name
       t.datetime :appointment_date
     end
 end
